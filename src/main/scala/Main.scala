7dea741385da94c6be9255d183167ebd7b766623
sopka13 <sopka13@mail.ru>
2023-06-04 22:26:57 +0300

    second commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+
+/**
+ * Backlog rsAgent
+ *  - 1. Add this project in GIT
+ *  - 2. Get design for documentations
+ */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("rsAgent start")
+  }
+}
